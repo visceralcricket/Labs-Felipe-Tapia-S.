9b27196a9ecf2fdc3412d0e23ee14ca770c639c4
Your Name <you@example.com>
2026-03-19 19:50:33 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -40,14 +40,14 @@ Recuerda que la lista almacena punteros, por lo que
 debes reservar memoria para cada elemento que agregues.
 Al finalizar retorna la lista creada.
 */
-
 List* crea_lista() {
    List* L = create_list();
+   /*
    int valores[10];
-   for(int i=0; i<=10; i++) {
+   for(int i=0; i<10; i++) {
       valores[i]=i+1;
       pushBack(L, &valores[i]);  
-   }
+   } */
    return L;
 }
 
@@ -56,15 +56,16 @@ Ejercicio 2.
 Crea una función que reciba una lista de enteros (int*) y 
 retorne la suma de sus elementos.
 */
-int sumaLista(List *L) {
 
-   int sumaTotal = 0, totalSize = get_size(L);
-   int *valor;
-   for(int i=0; i<totalSize; i++) {
-      valor=first(L);
-      sumaTotal+= (*valor);
-      next(L);
-   }
+int sumaLista(List *L) {
+   
+   int sumaTotal = 0;
+   /*
+   int* valor = first(L);
+   while(valor!=NULL) {
+      sumaTotal+=(*valor);
+      valor = next(L);
+   } */
    return sumaTotal;
 }
 
@@ -78,12 +79,13 @@ posiciona en el elemento anterior.
 */
 
 void eliminaElementos(List*L, int elem){
-   int totalSize = get_size(L);
-   for(int i=0; i<totalSize; i++) {
-      if(L->current->data==elem) {
+   
+   int* dato = first(L);
+   while(dato!=NULL) {
+      if((*dato)==elem) {
          popCurrent(L);
       }
-      else L->current=next(L);
+      dato = next(L);
    }
 }
 
@@ -95,8 +97,9 @@ Puedes usar una pila auxiliar.
 */
 
 void copia_pila(Stack* P1, Stack* P2) {
-
-   Stack* P3 = create_stack(); // <- Pila auxiliar (P3)
+   printf("f\n");
+   //Stack* auxiliar = create_stack(); // <- Pila auxiliar (P3)
+   
 }
 
 
@@ -108,14 +111,15 @@ paraéntesis balanceados. Retorna 1 si están balanceados,
 */
 
 int parentesisBalanceados(char *cadena) {
+
    int balanceo = 0;
    for(int i=0; cadena[i]!='\0'; i++) {
       if(cadena[i]=='(') balanceo++;
       else if(cadena[i]==')') balanceo--;
       
-      if(balanceo<0) return 0; // <- Verificar casos críticos como la cadena ")(" donde el balanceo se vuelve negativo.
+      if(balanceo<0) return 0; // <- Verificar casos críticos como ")("
    }
-   if(balanceo==0) return 1;
+   if(balanceo==0) return 1; 
    else return 0;
 }
 
