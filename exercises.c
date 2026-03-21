7fe3fcf2744ee8359dfc8fbea193cf307abacd13
visceralcricket <felipetapiasiede@gmail.com>
2026-03-21 14:13:17 -0300

    Refactor list creation and parentheses balancing logic
    Refactor functions to improve memory management and logic for parentheses balancing.

--- exercises.c
+++ exercises.c
@@ -42,12 +42,23 @@ Al finalizar retorna la lista creada.
 */
 List* crea_lista() {
    List* L = create_list();
-   
+   /*
+   Esto funciona pero es técnicamente incorrecto. Cuando el arreglo valores se libere del stack, las direcciones de memoria guardadas en L
+   tendrán comportamiento indefinido puesto que el arreglo valores se liberará tras finalizar la función crea_lista.
    int valores[10];
    for(int i=0; i<10; i++) {
       valores[i]=i+1;
       pushBack(L, &valores[i]);  
    }
+   */
+
+   for(int i=1; i<=10; i++) {
+      int* dato = (int*) malloc(sizeof(int));
+      if(dato) {
+         *dato = i;
+         pushBack(L, dato);
+      }
+   }   
    return L;
 }
 
@@ -58,10 +69,9 @@ retorne la suma de sus elementos.
 */
 
 int sumaLista(List *L) {
-   
    int sumaTotal = 0;
-   
    int* valor = first(L);
+   
    while(valor!=NULL) {
       sumaTotal+=(*valor);
       valor = next(L);
@@ -79,7 +89,6 @@ posiciona en el elemento anterior.
 */
 
 void eliminaElementos(List*L, int elem){
-   
    int* dato = first(L);
    while(dato!=NULL) {
       if((*dato)==elem) {
@@ -97,8 +106,16 @@ Puedes usar una pila auxiliar.
 */
 
 void copia_pila(Stack* P1, Stack* P2) {
-   // Stack* auxiliar = create_stack(); // <- Pila auxiliar (P3)
-   
+   Stack* auxiliar = create_stack(); // <- Pila auxiliar
+   void* dato;
+
+   while((dato=pop(P1)) != NULL) { // Vaciar P1 en auxiliar (invertido) mientras P1 siga lleno
+      push(auxiliar, dato);
+   }
+   while((dato=pop(auxiliar)) != NULL) { // Vaciar auxiliar y comenzar a llenar ambas P1 y P2 al mismo tiempo (orden original)
+      push(P1, dato);
+      push(P2, dato);
+   }
 }
 
 
@@ -110,18 +127,35 @@ paraéntesis balanceados. Retorna 1 si están balanceados,
 */
 
 int parentesisBalanceados(char *cadena) {
+   Stack *pila = create_stack(); // Crear una pila donde almacenar los paréntesis
 
-   int balanceo = 0;
-   // char parentesisAbiertos[3] = {'(', '[', '{'};
-   // char parentesisCerrados[3] = {')', ']', '}'};
-   
    for(int i=0; cadena[i]!='\0'; i++) {
-      if(cadena[i]=='(') balanceo++;
-      else if(cadena[i]==')') balanceo--;
-      
-      if(balanceo<0) return 0; // <- Verificar casos críticos como ")("
-   }
-   if(balanceo==0) return 1; 
-   else return 0;
-}
+      char caracter = cadena[i];
 
+      if(caracter=='(' || caracter=='[' || caracter=='{') { // _______________________
+         char *pChar = (char*) malloc(sizeof(char)); //_______|
+         *pChar = caracter;                         // | -> Guardar tipo de paréntesis abierto en la pila para verificar si se cierra después
+         push(pila, pChar);                        //  _______________________________
+      }
+      else if(caracter==')' || caracter==']' || caracter=='}') { // En caso de encontrar un paréntesis que cierra:
+         char *topCaracter = (char*) pop(pila); // Retirar el primer elemento de la pila
+         if(topCaracter==NULL) return 0;   // En caso de que la pila esté vacía, significa que falta un paréntesis abierto, por ende están desbalanceados
+
+         if((caracter==')' && *topCaracter != '(') ||   //_______________________
+            (caracter==']' && *topCaracter != '[') ||   //| Casos donde el paréntesis de cierre encontrado no coincide con el de apertura
+            (caracter=='}' && *topCaracter != '{')) {   //_______________________
+            free(topCaracter);   // Liberar la memoria reservada del paréntesis de cierre actual
+            return 0;   // Retornar -> paréntesis desbalanceados
+         }
+         free(topCaracter);
+      }
+   }
+   void *sobrante = pop(pila);
+   if(sobrante==NULL) return 1;// Pila de paréntesis abiertos vacía
+   // En caso contrario, quiere decir que aún hay paréntesis abiertos remanentes en la pila, ej: "((())"
+   while(sobrante!=NULL) {
+      free(sobrante);
+      sobrante= pop(pila);
+   }
+   return 0;
+}
